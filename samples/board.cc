9095231c7440312b2bdd89047007023ee58e489f
Arafo <rafa_marcen@hotmail.com>
2015-04-23 19:51:45 +0200

    #10

--- samples/board.cc
+++ samples/board.cc
@@ -3,9 +3,10 @@
 #endif
 
 #include <iostream>
+#include <time.h>
 
 #ifdef __APPLE__
-//#include <GLUT/glut.h>
+#include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #else
@@ -17,20 +18,20 @@
 #include "board.h"
 
 void cuadro();
-void peon();
+void peon(int i, int j);
 
 void board_init() 
 {
-	glClearColor(0.5, 0.5, 1.0, 0.0);
+	glClearColor(0, 0, 0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glFrustum(-2, 2, -1.5, 1.5, 1, 40);
     
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef(0, 0, -3);
-    glRotatef(50, 1, 0, 0);
-    glRotatef(70, 0, 1, 0);
+    //glLoadIdentity();
+    //glTranslatef(0, 0, -3);
+    //glRotatef(50, 1, 0, 0);
+    //glRotatef(70, 0, 1, 0);
 }
 
 void board_reshape(int w, int h)
@@ -62,23 +63,28 @@ void board_display(float x, float y, float z)
     // Suelo
     glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_LINES);
-    for (GLfloat i = -2.5; i <= 2.5; i += 0.25) {
-        glVertex3f(i, 0, 2.5); glVertex3f(i, 0, -2.5);
-        glVertex3f(2.5, 0, i); glVertex3f(-2.5, 0, i);
+    for (GLfloat i = -3; i <= 3*8; i += 0.3) {
+        glVertex3f(i, 0, 3.0f); glVertex3f(i, 0, -3.0f);
+        glVertex3f(3.0f, 0, i); glVertex3f(-3.0f, 0, i);
     }
     glEnd();
     
-    // Tablero de ajedrez
+    //int max_peones = 8;
+    //int peones = 0;
+    // Semilla
+    //srand (time(NULL));
+    
+    // Tablero de ajedrez 8x8
 	for (int i = 0; i < 8; i++)
 	{
 		glPushMatrix();
 		// Trasladar a la fila
-        glTranslatef(0.0f, 0.0f, -i*0.25f);
+        glTranslatef(0.0f, 0.0f, i*3.0f);
 		for (int j = 0; j < 8; j++)
 		{
 			glPushMatrix();
 			// Trasladar a la columna
-            glTranslatef(j*0.25f, 0.0f, 0.0f);
+            glTranslatef(j*3.0f, 0.0f, 0.0f);
 			if ((i + j) % 2)
 				// Color verde
 				glColor3f(0.0, 1.0, 0.0);
@@ -90,6 +96,9 @@ void board_display(float x, float y, float z)
 		}
 		glPopMatrix();
 	}
+    peon(0, 0);
+    peon(2, 2);
+    peon(0, 1);
 }
 
 void board_clean() 
@@ -101,41 +110,45 @@ void cuadro()
 {
     glBegin(GL_QUADS);
     glVertex3f(0.0f,0.0f,0.0f); // Vertice inferior izquierdo
-    glVertex3f(0.25f,0.0f,0.0f); // Vertice inferior derecho
-    glVertex3f(0.25f,0.0f,-0.25f); // Vertice superior derecho
-    glVertex3f(0.0f,0.0f,-0.25f); // Vertice superior izquierdo
+    glVertex3f(3.0f,0.0f,0.0f); // Vertice inferior derecho
+    glVertex3f(3.0f,0.0f,-3.0f); // Vertice superior derecho
+    glVertex3f(0.0f,0.0f,-3.0f); // Vertice superior izquierdo
     glEnd();
 }
 
-void peon() 
-{
-//    GLUquadricObj *quadric;
-//    quadric = gluNewQuadric();
-//    
-//    glColor3f(1.0f, 0.5f , 0.5f);
-//
-//    glPushMatrix();
-//    // Base
-//	glTranslatef(0.0f ,0.75f, 0.0f);
-//	gluSphere(quadric, 0.75f, 2, 10);
-//    
-//    // Cuerpo
-//	glTranslatef(0.0f, 1.5f, 0.0f);
-//	gluCylinder(quadric, 0.2f, 0.2f, 0.4f, 20, 20);
-//    
-//    // Cabeza
-//	glTranslatef(0.0f, 0.4f, 0.0f);
-//	gluSphere(quadric, 0.05f, 10, 10);
-//    glPopMatrix();
-    
-    GLUquadric *solid = gluNewQuadric();
+void peon(int x, int y) 
+{    
+    GLUquadric *quadric = gluNewQuadric();
     glColor3f(0.0f, 0.0f, 1.0f);
-	glPushMatrix();
-    glRotated(-90.0f, 1, 0, 0);
-    gluDisk(solid, 0, .75, 20, 1);
-    gluCylinder(solid, .5, 0, 3, 20, 1);
-    glTranslated(0,0,2.6);
-    gluSphere(solid, .4, 6, 6);
-	glPopMatrix();
     
+	glPushMatrix();
+    glTranslated(x*3.0f + 1.5f, 0.0f, y*3.0f - 1.5f);
+    glRotated(-90.0f, 1, 0, 0);
+    
+    // Base
+    gluCylinder(quadric, 1.25f, 1.25f, 0.4f, 20, 20);
+    
+    // Disco inferior
+    glTranslated(0.0f, 0.0f, 0.4f);
+    gluDisk(quadric, 0, 1.1f, 20, 1);
+    
+    // Cono
+    glTranslatef(0.0f ,0.0f, 0.4f);
+    gluCylinder(quadric, 1.0f, 0.3f, 1.0f, 20, 20);
+    
+    // Cilindro central
+    glTranslatef(0.0f, 0.0f, 1.0f);
+    gluCylinder(quadric, 0.3f, 0.3f, 1.5f, 20, 20);
+    
+    // Cabeza
+    glRotated(90.0f, 1, 0, 0);
+    glTranslatef(0.0f, 1.5f, 0.0f);
+    gluSphere(quadric, 0.6f, 20, 20);
+    
+    // Disco superior
+    glRotated(-90.0f, 1, 0, 0);
+    glTranslatef(0.0f, 0.0f, -0.6f);
+    gluDisk(quadric, 0, 0.6f, 20, 20);
+    
+	glPopMatrix();
 }
